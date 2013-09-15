a2d9bec59c90f448f2d6b62c3696c9c86c92c168
Charles Liu <cliu2014@mit.edu>
2013-09-15 15:06:14 -0400

    Remove State object, and document code for board

--- board.js
+++ board.js
@@ -1,3 +1,7 @@
+/*
+	The GameCoord object simply takes two numbers and stores an (x,y) tuple that represents the
+	coordinate of a cell in the overall game grid.
+*/
 var GameCoord = function(x, y) {
 	var that = Object.create(Board.prototype);
 	that.x = x;
@@ -5,23 +9,9 @@ var GameCoord = function(x, y) {
 	return that;
 };
 
-var State = function(width, height, alive) {
-	var that = Object.create(State.prototype);
-	that.width = width;
-	that.height = height;
-	that.alive = alive;
-	return that;
-};
-
-var Board = function(initState) {
+var Board = function(width, height, alive) {
 
 	var that = Object.create(Board.prototype);
-
-	// Read in the initial state
-	var width = initState.width;
-	var height = initState.height;
-	var initialAlive = initState.alive;
-
 	var board = [];
 
 	// Initialize a completely empty board
@@ -34,11 +24,25 @@ var Board = function(initState) {
 	});
 
 	// Set the appropriate cells on the board to be true
-	each(initialAlive, function(coord) {
+	each(alive, function(coord) {
 		board[coord.x][coord.y] = true;
 	});
 
+	/*
+		Helper function that gives the number of alive neighbors surrounding the cell at (x, y) according
+		to the rules of Game of Life. It takes into account the 8 surrounding cells (includes diagonal neighbors)
+		The inputs to the function x and y must be valid board coordinates
+		(i.e. nonnegative and no greater than the width or height of the board)
+
+		Note: At the edge of the board, cells that are "off the board" are always considered to be dead.
+		So, for example, the cell at (0, 0) - the top left corner - has at most 3 live neighbors
+		(right, below, and diagonally right/below)
+	*/
 	var getNumNeighbors = function(x, y) {
+
+		if (x < 0 || y < 0 || x >= width || y >= height) {
+			return 0;
+		}
 
 		var numNeighbors = 0;
 		from_to(Math.max(0, x-1), Math.min(width-1, x+1), function(i) {
@@ -52,23 +56,44 @@ var Board = function(initState) {
 		return numNeighbors;
 	};
 
+	/*
+		Returns the status of the cell at position (x, y). Requires that the inputs x and y be valid board coordinates
+		(i.e. nonnegative and no greater than the width or height of the board)
+
+		If out-of-bounds inputs are given then the response is always false
+	*/
 	that.isAlive = function(x, y) {
+		if (x < 0 || y < 0 || x >= width || y >= height) {
+			return false;
+		}
 		return board[x][y];
 	};
 
+	/*
+		Given a board state, return the board state at the next time step according to the rules passed into the function
+
+		The rules object must provide a public function isAliveNext that determines whether a cell is alive
+		at the next timestep depending on how many alive neighbors it has and whether the cell is alive
+		at the current timestep.
+
+		The rules to the game can be interchanged, as long as the function signature to isAliveNext remains the same.
+
+		This function returns a new Board object that represents the state of the board at the next timestep. The
+		existing board object is never modified.
+	*/
 	that.getNewBoardState = function(rules) {
 
-		var newInitAlive = [];
+		var newAlive = [];
 
 		from_to(0, width-1, function(i) {
 			from_to(0, height-1, function(j) {
 				if (rules.isAliveNext(getNumNeighbors(i, j), that.isAlive(i, j))) {
-					newInitAlive.push(GameCoord(i, j));
+					newAlive.push(GameCoord(i, j));
 				}
 			});
 		});
 
-		return Board(State(width, height, newInitAlive));
+		return Board(width, height, newAlive);
 	};
 
 	return that;
--- index.js
+++ index.js
@@ -1,18 +1,18 @@
 (function() {
 
 	var BLUE = Color(0,0,255);
+	var TIMESTEP = 500; // in ms
 
 	// create the drawing pad object and associate with the canvas
 	var padStart = Pad(document.getElementById('canvas-start'));
 	var pad = Pad(document.getElementById('canvas'));
 
-	var CELL_SIZE = 4;
+	var CELL_SIZE = 8;
 	var GAME_SIZE_X = pad.get_width() / CELL_SIZE;
 	var GAME_SIZE_Y = pad.get_height() / CELL_SIZE;
 	var rules = Rules();
 
-	var state = State(GAME_SIZE_X, GAME_SIZE_Y, Initialize(GAME_SIZE_X, GAME_SIZE_Y).getInitialConditions());
-	var board = Board(state);
+	var board = Board(GAME_SIZE_X, GAME_SIZE_Y, Initialize(GAME_SIZE_X, GAME_SIZE_Y).getInitialConditions());
 
 	var stepTime = function() {
 		drawBoard(board, pad);
@@ -31,7 +31,7 @@
 	};
 
 	drawBoard(board, padStart);
-	setInterval(stepTime, 500);
+	setInterval(stepTime, TIMESTEP);
 
 })();
 
